6f78fc15fb6511e0098003f77d641344968929df
rhodinemma <rhodinemma10@gmail.com>
2023-09-04 16:47:15 +0300

    feat: define items and news structs

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import(
 	"os"
 )
 
+type Item struct{
+	Title string
+	Link string
+	Traffic string
+	NewsItems []News
+}
+
+type News struct{
+	Headline string
+	HeadlineLink string
+}
+
 func main() {
 	readGoogleTrends
 }
