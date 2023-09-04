ce1d308adc970717dd064ff4d0ae60fd450af593
rhodinemma <rhodinemma10@gmail.com>
2023-09-04 18:36:42 +0300

    feat: add the unmarshal function from xml

--- main.go
+++ main.go
@@ -1,49 +1,68 @@
 package main
 
-import(
+import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
 
-type RSS struct{
+type RSS struct {
 	XMLName xml.Name `xml:"rss"`
 	Channel *Channel `xml:"channel"`
 }
 
-type Channel struct{
-	Title string `xml:"title"`
+type Channel struct {
+	Title    string `xml:"title"`
 	ItemList []Item `xml:"item"`
 }
 
-type Item struct{
-	Title string `xml:"title"`
-	Link string	`xml:"link"`
-	Traffic string `xml:"approx_traffic"`
+type Item struct {
+	Title     string `xml:"title"`
+	Link      string `xml:"link"`
+	Traffic   string `xml:"approx_traffic"`
 	NewsItems []News `xml:"news_item"`
 }
 
-type News struct{
-	Headline string `xml:"news_item_title"`
+type News struct {
+	Headline     string `xml:"news_item_title"`
 	HeadlineLink string `xml:"news_item_url"`
 }
 
 func main() {
 	var r RSS
+
 	data := readGoogleTrends()
+
+	err := xml.Unmarshal(data, &r)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
 }
 
 func getGoogleTrends() *http.Response {
 	resp, err := http.Get("https://trends.google.com/trends/trendingsearches/daily/rss?geo=US")
 
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	return resp
 }
 
 func readGoogleTrends() []byte {
-	getGoogleTrends
+	resp := getGoogleTrends()
+
+	data, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return data
 }
