ec79feecbcfb441490df2eb2aea617de51637ae4
rhodinemma <rhodinemma10@gmail.com>
2023-09-04 16:29:27 +0300

    chore: add base files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rhodinemma/google-trends-project
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	readGoogleTrends
+}
+
+func readGoogleTrends() {
+	getGoogleTrends
+}
+
+func getGoogleTrends() {
+
+}
